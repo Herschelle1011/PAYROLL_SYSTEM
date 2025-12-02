3b1360ef0d31cad0837c8ff5cd1b5130de83d0e4
Louis Padigos <louispadigos145@gmail.com>
2025-12-02 16:05:21 +0800

    test

---
+++ PAYROLL SYSTEM OJT/Pages/Admin/dashboard.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PAYROLL_SYSTEM_OJT.Pages.Admin
+{
+    public class dashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,45 @@
+﻿create database RakSonOPC;
+use RakSonOPC;
+
+--Setup
+create table Users (
+UserID int not null identity(1,1) primary key,
+Username varchar(50) not null,
+PasswordHash varchar(255) not null,
+Email varchar(100) not null,
+);
+create table Employee (
+EmployeeID int not null identity (1,1) primary key,
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Position varchar(50) not null,
+);
+--create table Company ();
+--create table Mandatory ();
+--create table ShiftCodes ();
+--create table DayType ();
+--create table OtherIncome ();
+--create table OtherDeduction ();
+--create table SystemTable ();
+
+----Activities
+--create table DTR ();
+--create table LeaveApplication ();
+--create table OvertimeApplication ();
+--create table Loans ();
+--create table PayrollOtherIncome ();
+--create table PayrollOtherDeduction ();
+--create table LastWithholding ();
+--create table Payroll ();
+--create table ChangeShift ();
+
+----Reports
+--create table DTRReports ();
+--create table PayrollReports ();
+--create table MandatoryReports ();
+--create table WithholdingTaxReports ();
+--create table LeaveReports ();
+--create table LoanReports ();
+--create table AccountingReports ();
+--create table ATM_Bank ();
+--create table Demographics ();
